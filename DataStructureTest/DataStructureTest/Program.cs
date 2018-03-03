58beb44e033502bf417c2911ddda22e19bde61d3
Carol <475880692@qq.com>
2018-03-03 13:43:18 +0800

    program 9*9

---
+++ DataStructureTest/DataStructureTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row, column;
+            for (row = 1; row <= 9; row++)
+            {
+                for (column = 1; column <= row; column++)
+                {
+                    Console.Write(column + "*" + row + "=" + column * row);
+                    Console.Write(" ");
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
