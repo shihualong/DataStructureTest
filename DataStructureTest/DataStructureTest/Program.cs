e246da6ffb898c411f0f39ea283d8b61f0792d4b
Carol <475880692@qq.com>
2018-03-05 21:56:40 +0800

    删除脚本  ，清理代码

--- DataStructureTest/DataStructureTest/Program.cs
+++ DataStructureTest/DataStructureTest/Program.cs
@@ -10,16 +10,7 @@ namespace DataStructureTest
     {
         static void Main(string[] args)
         {
-            int row, column;
-            for (row = 1; row <= 9; row++)
-            {
-                for (column = 1; column <= row; column++)
-                {
-                    Console.Write(column + "*" + row + "=" + column * row);
-                    Console.Write(" ");
-                }
-                Console.WriteLine(" ");
-            }
+           
         }
     }
 }
