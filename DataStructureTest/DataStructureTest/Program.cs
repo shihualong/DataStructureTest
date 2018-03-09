86ef6b7fd86c88d9d81cbf7e2c0ef28e556a5e87
Carol <475880692@qq.com>
2018-03-09 14:16:55 +0800

    测试ArrayList

--- DataStructureTest/DataStructureTest/Program.cs
+++ DataStructureTest/DataStructureTest/Program.cs
@@ -10,7 +10,30 @@ namespace DataStructureTest
     {
         static void Main(string[] args)
         {
-           
+            MyArrayList arr = new MyArrayList();
+            Console.WriteLine("arr当前的容量为：" + arr.Capacity + "  长度为：" + arr.Count);
+
+            arr.Add(1);
+            Console.WriteLine("arr当前的容量为：" + arr.Capacity + "  长度为：" + arr.Count);
+
+            for (int i = 2; i < 6; i++)
+            {
+                arr.Add(i);
+            }
+            Console.WriteLine("arr当前的容量为：" + arr.Capacity + "  长度为：" + arr.Count);
+
+            arr.Deleted(arr.Count - 1);
+            for (int i = 0; i < arr.Count; i++)
+            {
+                Console.WriteLine("第" + i + "个元素为： " + arr[i].ToString());
+            }
+            Console.WriteLine("arr当前的容量为：" + arr.Capacity + "  长度为：" + arr.Count);
+
+            arr.TrimToSize();
+
+            Console.WriteLine("arr当前的容量为：" + arr.Capacity + "  长度为：" + arr.Count);
+
+            Console.ReadLine();
         }
     }
 }
