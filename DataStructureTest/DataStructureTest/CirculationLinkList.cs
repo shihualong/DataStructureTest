c0df3d59a40402706d5c03734c190c59f99e6d85
Carol <475880692@qq.com>
2018-03-09 22:15:38 +0800

    循环线性表

---
+++ DataStructureTest/DataStructureTest/CirculationLinkList.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureTest
+{   
+
+    class CirculationLinkList
+    {  
+        private class Node
+        {
+            public object item;
+            public CirculationLinkList.Node next;
+            public override string ToString()
+            {
+                return item.ToString();
+            }
+            public Node(object value)
+            {
+                item = value;
+            }
+            
+        }
+        private int count;
+
+        private Node tail;
+
+        private Node curPrev;
+
+        public int Count
+        {
+            get { return count; }
+        }
+        public void Add(object value)
+        {
+            Node newNode = new Node(value);
+            if (tail == null)
+            {
+                tail = newNode;
+                tail.next = newNode;
+                curPrev = newNode;
+            }
+            else
+            {
+                newNode.next = tail.next;
+                tail.next = newNode;
+                if (curPrev == tail)
+                {
+                    curPrev = newNode;
+                }
+                tail = newNode;
+            }
+            count++;
+        }
+
+        public void RemoveCurrNode()
+        {
+            if (tail == null)
+            {
+                ThrowNAOORE("null");
+            }
+            else if (count == 1)
+            {
+                tail = null;
+                curPrev = null;
+            }
+            else
+            {
+                if (curPrev.next == tail)
+                {
+                    tail = curPrev;
+                }
+                curPrev.next = curPrev.next.next;
+                count--;
+            }
+        }
+        public void Move(int step)
+        {
+            if (step < 0)
+            {
+                ThrowNAOORE("step");
+            }
+            if (tail == null)
+            {
+                ThrowNAOORE("null");
+            }
+            for (int i = 0; i < step; i++)
+            {
+                curPrev = curPrev.next;
+            }
+        }
+        public object CurrentNode
+        {
+            get { return curPrev.next.item; }
+        }
+        public override string ToString()
+        {
+            if (tail == null)
+            {
+                return string.Empty;
+            }
+            string s = "";
+            Node head = tail.next;
+            for (int i = 0; i < count; i++)
+            {
+                s += head.ToString() + "   ";
+                head = head.next;
+            }
+            return s;
+        }
+        public void ThrowNAOORE(string name)
+        {
+            string message = "";
+            StringBuilder sb = new StringBuilder("the" + name + message, 40);
+            switch (name)
+            {
+                case "null":
+                    message = "Node";
+                    throw new ArgumentOutOfRangeException(name, sb.ToString());
+                    break;
+                case "step":
+                    message = "is not less than zero";
+                    throw new ArgumentOutOfRangeException(name, sb.ToString());
+                    break;
+                case "capacity":
+                    message = "can not Null";
+                    throw new ArgumentOutOfRangeException(name, sb.ToString());
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
--- DataStructureTest/DataStructureTest/Program.cs
+++ DataStructureTest/DataStructureTest/Program.cs
@@ -10,30 +10,52 @@ namespace DataStructureTest
     {
         static void Main(string[] args)
         {
-            MyArrayList arr = new MyArrayList();
-            Console.WriteLine("arr当前的容量为：" + arr.Capacity + "  长度为：" + arr.Count);
+            #region ArrrayList
+            //MyArrayList arr = new MyArrayList();
+            //Console.WriteLine("arr当前的容量为：" + arr.Capacity + "  长度为：" + arr.Count);
 
-            arr.Add(1);
-            Console.WriteLine("arr当前的容量为：" + arr.Capacity + "  长度为：" + arr.Count);
+            //arr.Add(1);
+            //Console.WriteLine("arr当前的容量为：" + arr.Capacity + "  长度为：" + arr.Count);
 
-            for (int i = 2; i < 6; i++)
-            {
-                arr.Add(i);
-            }
-            Console.WriteLine("arr当前的容量为：" + arr.Capacity + "  长度为：" + arr.Count);
+            //for (int i = 2; i < 6; i++)
+            //{
+            //    arr.Add(i);
+            //}
+            //Console.WriteLine("arr当前的容量为：" + arr.Capacity + "  长度为：" + arr.Count);
 
-            arr.Deleted(arr.Count - 1);
-            for (int i = 0; i < arr.Count; i++)
-            {
-                Console.WriteLine("第" + i + "个元素为： " + arr[i].ToString());
-            }
-            Console.WriteLine("arr当前的容量为：" + arr.Capacity + "  长度为：" + arr.Count);
+            //arr.Deleted(arr.Count - 1);
+            //for (int i = 0; i < arr.Count; i++)
+            //{
+            //    Console.WriteLine("第" + i + "个元素为： " + arr[i].ToString());
+            //}
+            //Console.WriteLine("arr当前的容量为：" + arr.Capacity + "  长度为：" + arr.Count);
 
-            arr.TrimToSize();
+            //arr.TrimToSize();
 
-            Console.WriteLine("arr当前的容量为：" + arr.Capacity + "  长度为：" + arr.Count);
+            //Console.WriteLine("arr当前的容量为：" + arr.Capacity + "  长度为：" + arr.Count);
 
+            //Console.ReadLine(); 
+            #endregion
+            #region MyRegion
+            CirculationLinkList lst = new CirculationLinkList();
+            lst.Add(1);
+            lst.Add(2);
+            lst.Add(3);
+            lst.Add(4);
+            lst.Add(5);
+            lst.Add(6);
+            Console.WriteLine(lst.ToString());
+
+            Console.WriteLine(lst.CurrentNode.ToString());
+            lst.RemoveCurrNode();
+
+            Console.WriteLine(lst.ToString());
+            lst.Move(3);
+
+            Console.WriteLine(lst.ToString());
             Console.ReadLine();
+            #endregion
+            
         }
     }
 }
